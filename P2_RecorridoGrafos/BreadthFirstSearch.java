d53b16d78141e52e562d0c518b7116d3130382a6
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-11-22 22:14:13 -0500

    Se quito la clase Queue, ahora se usa clase Queue de Java

--- P2_RecorridoGrafos/BreadthFirstSearch.java
+++ P2_RecorridoGrafos/BreadthFirstSearch.java
@@ -1,18 +1,20 @@
 package P2_RecorridoGrafos;
 
 import java.awt.image.Raster;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BreadthFirstSearch {
 	
 	public static void search(Raster graph, int [][] comps, int x, int y, int rang) {
 		
-		Queue queue = new Queue();
-		queue.push(new Node(x, y));
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(new Node(x, y));
 		Range range = new Range(graph.getSample(x, y, 0), rang);
 		int grayScale = graph.getSample(x, y, 0);
 		
 		while(!queue.isEmpty()) {
-			Node current = queue.pop();
+			Node current = queue.remove();
 			// Se resuelve las colas
 			comps[current.y][current.x] = grayScale;
 			// Se forma colas
@@ -20,32 +22,32 @@ public class BreadthFirstSearch {
 		}
 	}
 	
-	public static void buildQueue(Queue queue, Node current, Raster graph, Range range, int [][] comp) {
+	public static void buildQueue(Queue<Node> queue, Node current, Raster graph, Range range, int [][] comp) {
 		
 		int x, y;
 		
 		x = current.x;
 		y = current.y - 1;
 		if(checkLimits(x, y, range, graph, comp)) {
-			queue.push(new Node(x, y));
+			queue.add(new Node(x, y));
 		}
 		
 		x = current.x + 1;
 		y = current.y;
 		if(checkLimits(x, y, range, graph, comp)) {
-			queue.push(new Node(x, y));
+			queue.add(new Node(x, y));
 		}
 		
 		x = current.x;
 		y = current.y + 1;
 		if(checkLimits(x, y, range, graph, comp)) {
-			queue.push(new Node(x, y));
+			queue.add(new Node(x, y));
 		}
 		
 		x = current.x - 1;
 		y = current.y;
 		if(checkLimits(x, y, range, graph, comp)) {
-			queue.push(new Node(x, y));
+			queue.add(new Node(x, y));
 		}
 		
 	}
@@ -58,32 +60,6 @@ public class BreadthFirstSearch {
 			return visited[y][x] == -1 && range.inTheRange(graph.getSample(x, y, 0));
 		}
 	}
-}
-
-class Queue{
-	
-	public Node front;
-	public Node behind;
-	
-	public void push(Node node) {
-		if(front == null)
-			front = node;
-		else {
-			front.next = node;
-			behind = node;
-		}
-	}
-	
-	public Node pop() {
-		Node popNode = front;
-		front = front.next;
-		return popNode;
-	}
-	
-	public boolean isEmpty() {
-		return front == null;
-	}
-	
 }
 
 
