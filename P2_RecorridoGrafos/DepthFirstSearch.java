1c7574549f8e0c40d5bef899eb5577a1e31c471e
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-11-23 08:47:13 -0500

    Se cambio a algoritmo de tipo iterativo
    El algoritmo recursivo no funcionaba correctamente

--- P2_RecorridoGrafos/DepthFirstSearch.java
+++ P2_RecorridoGrafos/DepthFirstSearch.java
@@ -1,81 +1,87 @@
 package P2_RecorridoGrafos;
 
 import java.awt.image.Raster;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class DepthFirstSearch {
 	
-	/*
-	 * Solo se toma en cuenta 4 vecinos
-	 * En una imagen matriz parte de tener x, y posicion
-	 * 
-	 * [P] -> (x, y) Nodo actual
-	 * 
-	 * Vecinos
-	 * [A] -> (x, y - 1)
-	 * [B] -> (x + 1, y)
-	 * [C] -> (x, y + 1)
-	 * [D] -> (x - 1, y)
-	 * 
-	 * 
-	 *      [A]
-	 *   [D][P][B]
-	 *      [C] 
-	 */
-	
-	public static void search(Raster graph, int x, int y, int[][] comp, int mark, int range) {
-		search(graph, new Node(x, y), comp, mark, new Range(graph.getSample(x, y, 0), range));
+	public static void search(Raster graph, int x, int y, int[][] comp, int gray, int range) {
+		int count = 0;
+		search(graph, new Node(x, y), comp, gray, new Range(graph.getSample(x, y, 0), range), count);
 	}
 	
-	private static void search(Raster graph, Node current, int[][] comp, int mark, Range range) {
+	private static void search(Raster graph, Node current, int[][] comp, int gray, Range range, int count) {
 		
-		comp[current.y][current.x] = mark;
-		Link linklist = buildLink(graph, range, current, comp);
+		comp[current.y][current.x] = gray;
+		Stack<LinkedList<Node>> stack = new Stack<LinkedList<Node>>();
+		stack.add(buildLink(graph, range, current, comp));
 		
-		while(!linklist.isEmpty()) {
-			search(graph, linklist.getNode(), comp, mark, range);
+		while(!stack.isEmpty()) {
+		
+			LinkedList<Node> tempList = stack.pop();
+			
+			while(!tempList.isEmpty()) {
+				Node node = tempList.remove();
+				if(canBeVisited(node.x, node.y, comp)) {
+					comp[node.y][node.x] = gray;
+					
+					if(!tempList.isEmpty()) {
+						System.out.println("Aun no estaba vacio");
+						stack.add(tempList);
+					}
+					
+					stack.add(buildLink(graph, range, node, comp));
+					break;
+				}
+			}
 		}
-
 	}
 	
-	private static Link buildLink(Raster graph, Range range, Node current, int[][] comp) {
-		Link link = new Link();
+	private static LinkedList<Node> buildLink(Raster graph, Range range, Node current, int[][] comp) {
+		
+		LinkedList<Node> link = new LinkedList<Node>();
 		int x, y;
 		
 		x = current.x;
 		y = current.y - 1;
-		if(checkLimits(x, y, range, graph, comp)) {
-			link.addNode(new Node(x, y));
+		if(checkLimits(x, y, range, graph) && canBeVisited(x, y, comp)) {
+			link.add(new Node(x, y));
 		}
 		
 		x = current.x + 1;
 		y = current.y;
-		if(checkLimits(x, y, range, graph, comp)) {
-			link.addNode(new Node(x, y));
+		if(checkLimits(x, y, range, graph) && canBeVisited(x, y, comp)) {
+			link.add(new Node(x, y));
 		}
 		
 		x = current.x;
 		y = current.y + 1;
-		if(checkLimits(x, y, range, graph, comp)) {
-			link.addNode(new Node(x, y));
+		if(checkLimits(x, y, range, graph) && canBeVisited(x, y, comp)) {
+			link.add(new Node(x, y));
 		}
 		
 		x = current.x - 1;
 		y = current.y;
-		if(checkLimits(x, y, range, graph, comp)) {
-			link.addNode(new Node(x, y));
+		if(checkLimits(x, y, range, graph) && canBeVisited(x, y, comp)) {
+			link.add(new Node(x, y));
 		}
 		
 		return link;
 	}
 	
 	// chequea la posicion de x e y de un nodo para que no se salga del limite
-	private static boolean checkLimits(int x, int y, Range range, Raster graph, int [][] visited) {
+	private static boolean checkLimits(int x, int y, Range range, Raster graph) {
 		// El nodo no se usa si no esta en el rango o esta fuera de los limites
 		if(x > graph.getWidth() - 1 || y > graph.getHeight() - 1 || x < 0 || y < 0) {
 			return false;
 		}else {
-			return visited[y][x] == -1 && range.inTheRange(graph.getSample(x, y, 0));
+			return range.inTheRange(graph.getSample(x, y, 0));
 		}
+	}
+	
+	private static boolean canBeVisited(int x, int y, int [][] visited) {
+		return visited[y][x] == -1;
 	}
 }
 
@@ -83,8 +89,6 @@ class Node{
 	
 	public int x;
 	public int y;
-	public boolean deactive;
-	public Node next;
 	
 	Node(int x, int y) {
 		this.x = x;
@@ -105,27 +109,5 @@ class Range{
 		
 	public boolean inTheRange(int number) {
 		return number >= inf && number <= sup;
-	}
-}
-
-class Link {
-	public Node head;
-	
-	public void addNode(Node node) {
-		if(head == null) {
-			head = node;
-		}else {
-			node.next = node;
-		}
-	}
-	
-	public Node getNode() {
-		Node node = head;
-		head = head.next;
-		return node;
-	}
-	
-	public boolean isEmpty() {
-		return head == null;
 	}
 }
