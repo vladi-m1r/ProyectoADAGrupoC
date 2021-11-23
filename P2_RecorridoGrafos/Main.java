a65d8e7c9c00643a8ebc9d27ba36eaf2324933e4
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-11-22 22:08:41 -0500

    Se incluyo nuevas funciones para mostrar la salida de imagen

--- P2_RecorridoGrafos/Main.java
+++ P2_RecorridoGrafos/Main.java
@@ -1,66 +1,67 @@
 package P2_RecorridoGrafos;
 
-import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 	public static void main(String[] args) {
-		testBreadthFirstSearch();
+		BufferedImage image = null;
+		
+		try {
+			image = ImageIO.read(new File("src/P2_RecorridoGrafos/images/dory.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedImage depthImage = testDepthFirstSearch(image);
+		draw(depthImage, "DepthFirstSearch");
+		BufferedImage breadImage = testBreadthFirstSearch(image);
+		draw(breadImage, "BreadthFirstSearch");
 	}
 
-	public static void testDepthFirstSearch() {
+	public static BufferedImage testDepthFirstSearch(BufferedImage image) {
 		System.out.println("DepthFirstSearch");
-		try {
-			BufferedImage image = ImageIO.read(new File("src/P2_RecorridoGrafos/images/dory.jpg"));
-			Raster raster = image.getRaster();
-			int [][] componentes = generateComp(raster.getHeight(), raster.getWidth());
-			// Busqueda profunda llena una matriz de componentes.
-			int rango = 25;
-			
-			for (int y = 0; y < componentes.length; y++) {
-				for (int x = 0; x < componentes[0].length; x++) {
-					if(componentes[y][x] == -1) {
-						DepthFirstSearch.search(raster, x, y, componentes, raster.getSample(x, y, 0), rango);
-					}
-				}
+		
+		Raster raster = image.getRaster();
+		int [][] componentes = generateComp(raster.getHeight(), raster.getWidth());
+		// Busqueda profunda llena una matriz de componentes.
+		int rango = 25;
+		
+		for (int y = 0; y < componentes.length; y++) {
+			for (int x = 0; x < componentes[0].length; x++) {
+				if(componentes[y][x] == -1)
+					DepthFirstSearch.search(raster, x, y, componentes, raster.getSample(x, y, 0), rango);
 			}
-			
-			showComponents(componentes);
-			draw(componentes);
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		
+		return buildImage(componentes);
 	}
 	
-	public static void testBreadthFirstSearch() {
+	public static BufferedImage testBreadthFirstSearch(BufferedImage image) {
 		System.out.println("BreadthFirstSearch");
-		try {
-			BufferedImage image = ImageIO.read(new File("src/P2_RecorridoGrafos/images/dory.jpg"));
-			Raster raster = image.getRaster();
-			int [][] componentes = generateComp(raster.getHeight(), raster.getWidth());
-			// Busqueda profunda llena una matriz de componentes.
-			int rango = 75;
+		
+		Raster raster = image.getRaster();
+		int [][] componentes = generateComp(raster.getHeight(), raster.getWidth());
+		// Busqueda profunda llena una matriz de componentes.
+		int rango = 25;
 			
-			for (int y = 0; y < componentes.length; y++) {
-				for (int x = 0; x < componentes[0].length; x++) {
-					if(componentes[y][x] == -1) {
-						BreadthFirstSearch.search(raster, componentes, x, y, rango);
-					}
+		for (int y = 0; y < componentes.length; y++) {
+			for (int x = 0; x < componentes[0].length; x++) {
+				if(componentes[y][x] == -1) {
+					BreadthFirstSearch.search(raster, componentes, x, y, rango);
 				}
 			}
-			
-			showComponents(componentes);
-			draw(componentes);
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+			
+		return buildImage(componentes);
 	}
 	
 	// Prueba del cargado de imagen en escala de grises
@@ -89,20 +90,12 @@ public class Main {
 		}
 	}
 	
-	public static void draw(int [][] matriz) {
+	public static void draw(BufferedImage img, String title) {
 		JFrame jframe = new JFrame();
+		jframe.setTitle(title);
 		jframe.setVisible(true);
-		jframe.setSize(1500, 1500);
-		Canvas canvas = new Canvas();
-		jframe.add(canvas);
-		Graphics graph = canvas.getGraphics();
-		
-		for (int i = 0; i < matriz.length; i++) {
-			for (int j = 0; j < matriz[0].length; j++) {
-				graph.setColor(new Color(matriz[i][j], matriz[i][j], matriz[i][j]));
-				graph.fillRect(j, i, 1, 1);
-			}
-		}
+		jframe.add(new JLabel(new ImageIcon(img)));
+		jframe.pack();
 	}
 	
 	// Genera un arreglo bidimensional con el numero -1 por defecto en los espacios
@@ -116,5 +109,18 @@ public class Main {
 		}
 		
 		return comps;
+	}
+	
+	public static BufferedImage buildImage(int [][] rgb) {
+		
+		BufferedImage image = new BufferedImage(rgb[0].length, rgb.length, 5);
+		
+		for (int i = 0; i < rgb.length; i++) {
+			for (int j = 0; j < rgb[0].length; j++) {
+				Color color = new Color(rgb[i][j], rgb[i][j], rgb[i][j]);
+				image.setRGB(j, i, color.getRGB());
+			}
+		}
+		return image;
 	}
 }
